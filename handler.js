cbeb4c03608cdb4a420868ad4962ed55045498c1
dualcorebrain <prasadanurag336@gmail.com>
2021-06-21 23:17:31 +0100

    Started Adding Results section

--- handler.js
+++ handler.js
@@ -8,14 +8,44 @@ let playerScore = 0;
 let computerScore = 0;
 let currentRound = 0;
 
+let logging = document.getElementsByClassName("btn");
+console.log(logging);
 
-  document.getElementById("rock").addEventListener("click", function(e){
-    if(playerScore >=5 || computerScore >=5 || currentRound >= 5){
-      return;
-    }
-    else{
-      game(e);
-    }
+
+//BUTTON/IMAGE HOVER EFFECTS
+document.getElementById("rock").addEventListener("mouseenter", function(e){
+  document.querySelector("#rock-image").classList.add("images-visible");
+});
+document.getElementById("rock").addEventListener("mouseleave", function(e){
+  document.querySelector("#rock-image").classList.remove("images-visible");
+});
+
+
+document.getElementById("paper").addEventListener("mouseenter", function(e){
+  document.querySelector("#paper-image").classList.add("images-visible");
+});
+document.getElementById("paper").addEventListener("mouseleave", function(e){
+  document.querySelector("#paper-image").classList.remove("images-visible");
+});
+
+
+document.getElementById("scissor").addEventListener("mouseenter", function(e){
+  document.querySelector("#scissor-image").classList.add("images-visible");
+});
+document.getElementById("scissor").addEventListener("mouseleave", function(e){
+  document.querySelector("#scissor-image").classList.remove("images-visible");
+});
+
+
+//REDUCE THIS LINES OF CODE
+//https://flaviocopes.com/how-to-add-event-listener-multiple-elements-javascript/
+document.getElementById("rock").addEventListener("click", function(e){
+  if(playerScore >=5 || computerScore >=5 || currentRound >= 5){
+    return;
+  }
+  else{
+    game(e);
+  }
 });
 
 document.getElementById("paper").addEventListener("click", function(e){
@@ -36,17 +66,19 @@ document.getElementById("scissor").addEventListener("click", function(e){
   }
 });
 
+
+
+
 function game(e){
   //play 5 rounds
   //keep scores
 
-  console.log(e.target[id]);
+  //document.getElementById(`${e.target.id}`)
 
   let playerChoice = playerPlay(e);
   let computerChoice = computerPlay();
 
   let roundResult = gameRound(playerChoice, computerChoice);
-  console.log(roundResult);
 
   if(roundResult == "YOU WIN"){
     playerScore++;
@@ -55,18 +87,20 @@ function game(e){
     computerScore++;
   }
 
-  console.log("You Chose: " + playerChoice + " Computer Chose: " + computerChoice + "| | The Current score is: Player- " + playerScore + " Computer- " + computerScore);
+  let consoleResult = "You Chose: " + playerChoice + " Computer Chose: " + computerChoice + "| | The Current score is: Player- " + playerScore + " Computer- " + computerScore;
+  console.log(consoleResult);
 
+  roundResultsAppender(consoleResult);
 
-if(currentRound == 4){
-  if(playerScore > computerScore){
-    console.log("You win the game!");
+  if(currentRound == 4){
+    if(playerScore > computerScore){
+      console.log("You win the game!");
+    }
+    else{
+      console.log("You lose the game!");
+    }
   }
-  else{
-    console.log("You lose the game!");
-  }
-}
-currentRound++;
+  currentRound++;
 }
 
 
@@ -90,6 +124,24 @@ function playerPlay(e){
 
 }
 
+function roundResultsAppender(consoleResult){
+    
+  let resultElement = document.createElement("h2");
+  let resultElementText = document.createTextNode(`${consoleResult}`);
+
+  resultElement.appendChild(resultElementText);
+
+  document.getElementById("result-container").appendChild(resultElement);
+
+  if(currentRound == 4){
+    document.getElementById("result-container").removeChild([0]);
+  }
+  return;
+}
+
+function removeRoundResults(){
+
+}
 
 function computerPlay(){
 //return randomly generated rock/paper/scissor   
